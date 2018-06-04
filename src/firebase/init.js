9a4a6f09aa678365e710753f758abf6e34e1f5ad
detautama <detautama11@gmail.com>
2018-06-04 12:21:15 +0800

    Installing firebase

---
+++ src/firebase/init.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAaLSqU90HXyQQWHdHDn1bWdnf0w2lZcBM",
+  authDomain: "udemy-ninja-smoothies-9c267.firebaseapp.com",
+  databaseURL: "https://udemy-ninja-smoothies-9c267.firebaseio.com",
+  projectId: "udemy-ninja-smoothies-9c267",
+  storageBucket: "udemy-ninja-smoothies-9c267.appspot.com",
+  messagingSenderId: "621979635944"
+};
+const firebaseApp = firebase.initializeApp(config);
+
+// export firestore database
+export default firebaseApp.firestore()
