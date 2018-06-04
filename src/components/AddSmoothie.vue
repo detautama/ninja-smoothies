6b07021f24bdfd46aed0d5f56b670267d4a3b0fe
detautama <detautama11@gmail.com>
2018-06-04 14:08:59 +0800

    Install Slug and saving record to firestore

--- src/components/AddSmoothie.vue
+++ src/components/AddSmoothie.vue
@@ -24,6 +24,9 @@
 </template>
 
 <script>
+import db from '@/firebase/init'
+import slugify from 'slugify'
+
 export default {
   name: 'AddSmoothie',
   data () {
@@ -31,12 +34,32 @@ export default {
       title: null,
       another: null,
       ingredients: [],
-      feedback: null
+      feedback: null,
+      slug: null
     }
   },
   methods: {
     AddSmoothie () {
-      console.log(this.title, this.ingredients)
+      if (this.title) {
+        this.feedback = null
+        // create slug
+        this.slug = slugify(this.title, {
+          replacement: '-',
+          remove: /[$*_+~>()'"!\-:@]/g,
+          lower: true
+        })
+        db.collection('smoothies').add({
+          title: this.title,
+          ingredients: this.ingredients,
+          slug: this.slug
+        }).then(() => {
+          this.$router.push({ name: 'Index'})
+        }).catch(err => {
+          console.log(err)
+        })
+      } else {
+        this.feedback = 'You must enter a smoothie title'
+      }
     },
     addIng () {
       if (this.another) {
