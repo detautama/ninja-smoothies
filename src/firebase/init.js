5ef8c5274310c58cfcc6902076455f289160188b
detautama <detautama11@gmail.com>
2018-06-04 12:28:43 +0800

    Update settings for version

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -11,6 +11,7 @@ var config = {
   messagingSenderId: "621979635944"
 };
 const firebaseApp = firebase.initializeApp(config);
+firebaseApp.firestore().settings({ timestampsInSnapshots: true})
 
 // export firestore database
 export default firebaseApp.firestore()
