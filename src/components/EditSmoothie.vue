500e19bf418dffe2da0dc4ec86036272176a3081
detautama <detautama11@gmail.com>
2018-06-06 17:06:41 +0800

    Added EditSmoothie component

---
+++ src/components/EditSmoothie.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="edit-smoothie container">
+    <h2>Edit a Smoothie {{ this.$route.params.slug }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EditSmoothie',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+
+
